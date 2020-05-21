4a9b056cb85828023304b122db1e103bebbd972c
tobyklauder <tobyklauder@hotmail.com>
2020-05-21 09:32:38 -0700

    Added UI, first tower path poison tower

--- final_game_demo/Assets/Scripts/GameManager.cs
+++ final_game_demo/Assets/Scripts/GameManager.cs
@@ -1,19 +1,87 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; 
 public class GameManager : MonoBehaviour
 {
-    public static string current = "basic"; 
-    // Start is called before the first frame update
-    void Start()
+    public static GameObject selected; 
+    public int money = 500; 
+    public Text onebuttontext;
+    public Text twobuttontext;
+    public Text towertypetext;
+    public static string current = "basic";
+    public static string towertype = "";
+
+    void Update()
     {
+        towertypetext.text = towertype;
+        if (towertype == "basic") {
+            if (selected.GetComponent<basictower>().pathone == 0) {
+                onebuttontext.text = "Double Range (75)"; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 1) {
+                onebuttontext.text = "Double Range (150)"; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 2) {
+                onebuttontext.text = "Slow Bugs 1/4 (400)"; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 3) {
+                onebuttontext.text = "Up Range, Slow (500)"; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 4) {
+                onebuttontext.text = "MAX LEVEL"; 
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void click() { 
+        if(towertype == "basic")
+        {
+            if (selected.GetComponent<basictower>().pathone == 0) {
+                selected.GetComponent<basictower>().radius += 5;
+                money -= 500;
+                selected.GetComponent<basictower>().pathone++;
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 1) {
+                selected.GetComponent<basictower>().radius += 10;
+                money -= 1250;
+                selected.GetComponent<basictower>().pathone++;
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 2) {
+                selected.GetComponent<basictower>().radius += 15;
+                selected.GetComponent<basictower>().slowenemy = true; 
+                money -= 1750;
+                selected.GetComponent<basictower>().pathone++;
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathone == 3) {
+                selected.GetComponent<basictower>().radius += 20;
+                money -= 2500;
+                selected.GetComponent<basictower>().pathone++;
+                return; 
+            }
+        }
+    }
+
+    public void clicktwo() {
+        if (towertype == "basic") {
+            if (selected.GetComponent<basictower>().pathtwo == 0) {
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathtwo == 1) {
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathtwo == 2) {
+                return; 
+            }
+            if (selected.GetComponent<basictower>().pathtwo == 3) {
+                return; 
+            }
+        }
     }
 }
+
--- final_game_demo/Assets/Scripts/basictower.cs
+++ final_game_demo/Assets/Scripts/basictower.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class basictower : MonoBehaviour
 {
+    public int radius = 5;
+    public bool slowenemy;
+    public int damage;
+    public int view = 0; 
+    public int pathone = 0;
+    public int pathtwo = 0; 
     private void Update()
     {
-        
+
     }
 
     private void Start()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        GameManager.selected = this.gameObject; 
+        Debug.Log("clicked");
+        GameManager.towertype = "basic"; 
     }
 }
