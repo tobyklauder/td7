a744750a76176262a3b9a3bce84e5070b0372467
Younget <48969941+Younget@users.noreply.github.com>
2020-05-25 23:38:14 -0700

    Screen Shake and Scene Switch
    -Ethan

---
+++ final_game_demo/Assets/Scripts/WaveSystem/CameraShake.cs
@@ -0,0 +1,60 @@
+﻿/*  CameraShake.cs
+ *  By: Ethan D. Young
+ *  Last Edited: 5/21/2020
+ *  Description: Make a screen shake juice effect for when enemies collide with target
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Camera mainCam;
+
+    float shakeAmount = 0;
+
+    void Awake()
+    {
+        if (mainCam == null)
+        {
+            mainCam = Camera.main;
+        }
+    }
+
+    public void Shake (float amt, float length)
+    {
+        shakeAmount = amt;
+        InvokeRepeating("BeginShake", 0, 0.01f);
+        Invoke("StopShake", length);
+    }
+
+    void BeginShake()
+    {
+        if (shakeAmount > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        mainCam.transform.localPosition = Vector3.zero;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Shake(0.1f, 0.2f);
+    }
+}
--- final_game_demo/Assets/Scripts/WaveSystem/EnemySpawnerScript.cs
+++ final_game_demo/Assets/Scripts/WaveSystem/EnemySpawnerScript.cs
@@ -1,57 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawnerScript : MonoBehaviour
 {
-     public GameObject Enemy;
-     public GameObject Enemy2;
-     public GameObject Enemy3;
+    public GameObject Enemy;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
     //float randX;
     //float randY;
     Vector2 whereToSpawn;
-     public float WaveCounter;
-     public int WaveMax;
-     public int EnemyCounter;
-     int i;
-     int p1;
-     int p2;
-     int p3;
-     //public float x1;
-     //public float x2;
-     //public float y1;
-     //public float y2;
+    public float WaveCounter;
+    public int WaveMax;
+    public int EnemyCounter;
+    int i;
+    int p1;
+    int p2;
+    int p3;
+    //public float x1;
+    //public float x2;
+    //public float y1;
+    //public float y2;
 
     // Start is called before the first frame update
     void Start()
     {
+        //SET IT SO THAT THIS ALL STARTS IF THE PLAYER CLICKS A BUTTON
         WaveCounter = 0;
         WaveMax = 20;
         WaveSystem();
         p3 = 3;
-        Debug.Log("P3 GOOD" + p3);
         p2 = 2;
         p1 = 3;
     }
 
     void WaveSystem()
     {
-        
-        if (WaveCounter <= 20)  
-        {  
+
+        if (WaveCounter <= 20)
+        {
             whereToSpawn = new Vector2(-11, 4);
-            Debug.Log("SPAWN POINT SET");
-            
+
             if (WaveCounter <= 6)
             {
-                Debug.Log("Wave Counter checked");
-                for (i=0; i < p3; i++)
+                for (i = 0; i < p3; i++)
                 {
                     Instantiate(Enemy, whereToSpawn, Quaternion.identity);
-                    Debug.Log("SPAWN SUCCESS");
                 }
                 p3 += 3;
-                Debug.Log("SPAWN POINT SET" + p3);
             }
 
             else if (WaveCounter <= 12)
@@ -67,7 +64,7 @@ public class EnemySpawnerScript : MonoBehaviour
                 {
                     for (i = 0; i <= p1; i++)
                     {
-                        Instantiate(Enemy2, whereToSpawn, Quaternion.identity); 
+                        Instantiate(Enemy2, whereToSpawn, Quaternion.identity);
                     }
                     p1 += 1;
 
@@ -106,7 +103,7 @@ public class EnemySpawnerScript : MonoBehaviour
                 }
                 else if (WaveCounter > 14)
                 {
-                    for (i = 0; i <= p1-1; i++)
+                    for (i = 0; i <= p1 - 1; i++)
                     {
                         Instantiate(Enemy3, whereToSpawn, Quaternion.identity);
                     }
@@ -120,7 +117,7 @@ public class EnemySpawnerScript : MonoBehaviour
                     }
                     p2 += 2;
 
-                    if(WaveCounter >= 15)
+                    if (WaveCounter >= 15)
                     {
                         for (i = 0; i < p3; i++)
                         {
@@ -134,7 +131,7 @@ public class EnemySpawnerScript : MonoBehaviour
             {
                 Instantiate(Enemy2, whereToSpawn, Quaternion.identity);
             }
-            
+
             //Instantiate(Enemy, whereToSpawn, Quaternion.identity);
         }
     }
@@ -144,12 +141,16 @@ public class EnemySpawnerScript : MonoBehaviour
     {
         //Set an enemy counter to determine when to start next wave
         EnemyCounter = GameObject.FindObjectsOfType<enemy>().Length;
+        if (WaveCounter >= 21)
+        {
+            SceneManager.LoadScene(0);
+        }
 
-        if (EnemyCounter == 0)
+        else if (EnemyCounter == 0)
         {
             WaveCounter++;
             WaveSystem();
-            
+
             if (WaveCounter == 12)
             {
                 p3 = 3;
